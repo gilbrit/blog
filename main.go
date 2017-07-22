3b93e14cd770afe86c13975716ee0b9884c89400
Tom Gilbert <tom@linuxbrit.co.uk>
2017-07-22 15:39:19 +0100

    Fix router setup

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"github.com/astaxie/beego"
-	"github.com/gilbrit/blog/controllers"
 	_ "github.com/gilbrit/blog/routers"
 )
 
 func main() {
-	beego.Router("/", &controllers.MainController{})
-	beego.Router("/gallery", &controllers.GalleryController{})
-	beego.Router("/details", &controllers.DetailsController{})
-	beego.Router("/contact", &controllers.ContactController{})
+	// beego.Router("/", &controllers.MainController{})
+	// beego.Router("/gallery", &controllers.GalleryController{})
+	// beego.Router("/details", &controllers.DetailsController{})
+	// beego.Router("/contact", &controllers.ContactController{})
 	beego.Run()
 }
--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,13 @@
 package routers
 
 import (
-	"github.com/gilbrit/blog/controllers"
 	"github.com/astaxie/beego"
+	"github.com/gilbrit/blog/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/gallery", &controllers.GalleryController{})
+	beego.Router("/details", &controllers.DetailsController{})
+	beego.Router("/contact", &controllers.ContactController{})
 }
